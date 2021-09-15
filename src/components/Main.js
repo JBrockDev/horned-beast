45cb00859ae62bd2fcbb6fcad674d4d2e1d16bec
jbrockdev <jbrockwork33@osgsmail.icu>
2021-09-15 14:41:38 -0700

    changed to map from foreach

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,17 @@
 import { Component } from "react";
 import HornedBeasts from "./HornedBeasts";
-import CardColumns from 'react-bootstrap/CardColumns';
 import Row from 'react-bootstrap/Row';
 
 
 class Main extends Component {
   render() {
-    getBeasts(this.props.items);
-    return <>{getBeasts(this.props.items)}</>;
+    return( 
+      <Row xs={1} md={2} lg={3}>
+        {this.props.items.map((beast, index) =>
+          <HornedBeasts key={"Beast-" + index} item={beast} />
+        )}
+      </Row>);
   }
 }
-
-function getBeasts(items) {
-  let beastList = [];
-  items.forEach((beast, index) => {
-    beastList.push(<HornedBeasts key={"Beast-" + index} item={beast} />);
-  });
-  return (
-    <Row xs={1} md={2} lg={3}>
-      {beastList}
-    </Row>
-  );
-};
 
 export default Main;
